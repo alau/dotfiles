bf13a70e33f4635db576117d0e3fbb2e32b47dea
Andreas Launila <github@lokorin.org>
2012-10-06 16:17:42 +0200

    Add a couple of missing keyboard options

--- keyboard/configure-gnome.sh
+++ keyboard/configure-gnome.sh
@@ -1,2 +1,2 @@
 gsettings set org.gnome.libgnomekbd.keyboard layouts "['us\tdvp', 'se']"
-gsettings set org.gnome.libgnomekbd.keyboard options "['caps\tcaps:swapescape']"
+gsettings set org.gnome.libgnomekbd.keyboard options "['caps\tcaps:swapescape', 'Compose key\tcompose:102', 'keypad\tkeypad:atm']"
