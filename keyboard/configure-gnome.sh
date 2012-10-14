3355cf8e806e94b26638db37cc754498aaa96fdd
Andreas Launila <github@lokorin.org>
2012-10-14 21:40:49 +0200

    Use right alt key to trigger third keyboard level

--- keyboard/configure-gnome.sh
+++ keyboard/configure-gnome.sh
@@ -1,2 +1,2 @@
 gsettings set org.gnome.libgnomekbd.keyboard layouts "['us\tdvp', 'se']"
-gsettings set org.gnome.libgnomekbd.keyboard options "['caps\tcaps:swapescape', 'Compose key\tcompose:102']"
+gsettings set org.gnome.libgnomekbd.keyboard options "['caps\tcaps:swapescape', 'Compose key\tcompose:102', 'lv3\tlv3:ralt_switch']"
