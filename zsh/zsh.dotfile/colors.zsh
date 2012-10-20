e65b1af8996f748d06c24c78a9e4e2b28b6d0b67
Andreas Launila <github@lokorin.org>
2012-10-20 20:35:21 +0200

    Add solarized color scheme for gnome
    Gnome conversion courtesy of
    http://www.sharms.org/blog/2012/08/gnome-terminal-color-schemes/

---
+++ zsh/zsh.dotfile/colors.zsh
@@ -0,0 +1,8 @@
+# Solarized dark color theme
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/foreground_color --type string "#708183"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_color --type string "#001E26"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/bold_color --type string "#81908F"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/palette --type string "#002731:#D01B24:#728905:#A57705:#2075C7:#C61B6E:#259185:#E9E2CB:#001E26:#BD3612:#465A61:#52676F:#708183:#5856B9:#81908F:#FCF4DC"
+
+# And better ls-colors
+eval `dircolors ~/.zsh/colors/dircolors-solarized/dircolors.ansi-dark`
