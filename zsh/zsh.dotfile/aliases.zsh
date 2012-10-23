1f517e8134241d167bebb3a3b07474d85a226c94
Andreas Launila <github@lokorin.org>
2012-10-23 21:44:06 +0200

    Add gff and gm alias

--- zsh/zsh.dotfile/aliases.zsh
+++ zsh/zsh.dotfile/aliases.zsh
@@ -4,4 +4,6 @@ alias gd='git diff --no-ext-diff'
 alias gaa='git add . && g'
 alias gc='git commit --verbose'
 alias gf='git commit --all --amend'
+alias gff='git pull --ff-only'
+alias gm='git merge --no-ff'
 unalias gs
