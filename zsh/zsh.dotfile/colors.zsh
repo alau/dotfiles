570f1e20b7b80345c9052c064f6702f7e17e32e8
Andreas Launila <github@lokorin.org>
2012-10-23 22:22:22 +0200

    Color partial completions
    Taken from http://www.reddit.com/r/zsh/comments/msps0/color_partial_tab_completions_in_zsh/c367xqo

--- zsh/zsh.dotfile/colors.zsh
+++ zsh/zsh.dotfile/colors.zsh
@@ -6,3 +6,6 @@ gconftool-2 --set /apps/gnome-terminal/profiles/Default/palette --type string "#
 
 # And better ls-colors
 eval `dircolors ~/.zsh/colors/dircolors-solarized/dircolors.ansi-dark`
+
+# Color partial completions
+zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)(?)*==34=00}:${(s.:.)LS_COLORS}")';
