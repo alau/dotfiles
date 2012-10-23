0ad1c6a3d5dd6725220756eec06b3a6f7f707e14
Andreas Launila <github@lokorin.org>
2012-10-23 22:05:03 +0200

    Add some cd aliases
    Nicked them from Nils :)

--- zsh/zsh.dotfile/aliases.zsh
+++ zsh/zsh.dotfile/aliases.zsh
@@ -8,3 +8,8 @@ alias gff='git pull --ff-only'
 alias gm='git merge --no-ff'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 unalias gs
+
+# cd
+alias u='cd ..'
+alias uu='cd ../..'
+alias uuu='cd ../../..'
