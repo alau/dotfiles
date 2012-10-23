3dc3e84b412337ddd15762b9bd55a3dbc233d938
Andreas Launila <github@lokorin.org>
2012-10-23 21:52:09 +0200

    Add cooler git log format
    Taken from http://coderwall.com/p/euwpig

--- zsh/zsh.dotfile/aliases.zsh
+++ zsh/zsh.dotfile/aliases.zsh
@@ -6,4 +6,5 @@ alias gc='git commit --verbose'
 alias gf='git commit --all --amend'
 alias gff='git pull --ff-only'
 alias gm='git merge --no-ff'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 unalias gs
