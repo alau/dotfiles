6ba03ce796560974e3f6debd44162f56887598f7
Andreas Launila <github@lokorin.org>
2012-10-24 15:08:31 +0200

    Add git aliases for pruning branches

--- zsh/zsh.dotfile/aliases.zsh
+++ zsh/zsh.dotfile/aliases.zsh
@@ -8,6 +8,8 @@ alias gf='git commit --all --amend'
 alias gff='git pull --ff-only'
 alias gm='git merge --no-ff'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias git-prune="git branch --merged | grep -v 'master$' | grep -v 'production$' | xargs git branch -d"
+alias git-prune-remote="git remote prune origin"
 unalias gs
 
 # cd
