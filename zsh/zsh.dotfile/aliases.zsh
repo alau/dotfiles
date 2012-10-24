7d174c13d7c2b815656282232f4cc9194cc47d7f
Andreas Launila <github@lokorin.org>
2012-10-24 10:37:06 +0200

    Add gb alias

--- zsh/zsh.dotfile/aliases.zsh
+++ zsh/zsh.dotfile/aliases.zsh
@@ -1,6 +1,7 @@
 # Git
 alias g='git status --short'
 alias gd='git diff --no-ext-diff'
+alias gb='git checkout --track origin -b'
 alias gaa='git add . && g'
 alias gc='git commit --verbose'
 alias gf='git commit --all --amend'
