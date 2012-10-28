ac072620808472dd91b8e54e43a688be9b6feaa0
Andreas Launila <github@lokorin.org>
2012-10-28 13:06:18 +0100

    Add dotfile for xfce terminal

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ cd "$(dirname "$0")"
 
 git pull && git submodule init && git submodule update
 
+# Symlink files ending in .dotfile
 for SOURCE in `ls -d1 $PWD/**/*.dotfile`
 do
   BASE=$(basename "$SOURCE")
@@ -12,10 +13,25 @@ do
   fi
 done
 
-touch ~/.ssh/autoloaded-identities
+# Symlink ordinary files in directories ending in .partial-dotfile
+for DIR in `ls -d1 $PWD/**/*.partial-dotfile`
+do
+  BASE=$(basename "$DIR")
+  for SOURCE in `find $DIR -type f`
+  do
+    DOTFILE="$HOME/.${BASE%.*}/${SOURCE##$DIR/}"
+    if ! [[ -L $DOTFILE && `readlink -f $DOTFILE` == $SOURCE ]]; then
+      mkdir -p ${DOTFILE%/*}
+      ln -s -v -n $SOURCE $DOTFILE
+    fi
+  done
+done
 
+# Create an empty file for zsh ssh-agent
+touch ~/.ssh/autoloaded-identities
+  # Gnome-spcific configuration
 if [ "$(pidof gnome-session)" ]; then
-  # Running gnome
+  # Gnome-specific configuration
   echo "[Gnome] Configuring keyboard"
   ./keyboard/configure-gnome.sh
 fi
