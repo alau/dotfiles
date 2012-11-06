e68004285fb5ee86e1b38b3410abf153067c0095
Andreas Launila <github@lokorin.org>
2012-11-06 17:39:01 +0100

    Change terminal to roxterm

--- awesome/config.partial-dotfile/awesome/rc.lua
+++ awesome/config.partial-dotfile/awesome/rc.lua
@@ -37,7 +37,7 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "roxterm"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
