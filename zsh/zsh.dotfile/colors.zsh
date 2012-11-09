4792f3adbffd3e2f8b8fa874146bc0c936ede9c1
Andreas Launila <github@lokorin.org>
2012-11-09 17:19:21 +0100

    Remove solarized ls colors

--- zsh/zsh.dotfile/colors.zsh
+++ zsh/zsh.dotfile/colors.zsh
@@ -1,5 +1,2 @@
-# Better ls-colors
-eval `dircolors ~/.zsh/colors/dircolors-solarized/dircolors.ansi-dark`
-
 # Color partial completions
 zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)(?)*==34=00}:${(s.:.)LS_COLORS}")';
