9abe4bc7721ca92bc41c57d60d8b02a1601e8505
Andreas Launila <github@lokorin.org>
2012-11-25 16:39:06 +0100

    Remove unused configuration variables

--- awesome/awesome.config-dotfile/rc.lua
+++ awesome/awesome.config-dotfile/rc.lua
@@ -33,17 +33,7 @@ config = {}
 widgets = {}
 
 load_part("appearance")
-
--- This is used later as the default terminal and editor to run.
 config.terminal = "roxterm"
-editor = os.getenv("EDITOR") or "vim"
-editor_cmd = config.terminal .. " -e " .. editor
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
