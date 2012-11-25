de415721922e451ffcd5e707615a855ebe7c2820
Andreas Launila <github@lokorin.org>
2012-11-25 17:56:58 +0100

    Remove mouse interaction with task list

--- awesome/awesome.config-dotfile/widgets.lua
+++ awesome/awesome.config-dotfile/widgets.lua
@@ -17,46 +17,6 @@ mytaglist.buttons = awful.util.table.join(
   awful.button({ }, 4, awful.tag.viewnext),
   awful.button({ }, 5, awful.tag.viewprev)
 )
-local mytasklist = {}
-mytasklist.buttons = awful.util.table.join(
-  awful.button({ }, 1,
-    function (c)
-      if c == client.focus then
-        c.minimized = true
-      else
-        if not c:isvisible() then
-          awful.tag.viewonly(c:tags()[1])
-        end
-        -- This will also un-minimize
-        -- the client, if needed
-        client.focus = c
-        c:raise()
-      end
-    end
-  ),
-  awful.button({ }, 3,
-    function ()
-      if instance then
-        instance:hide()
-        instance = nil
-      else
-        instance = awful.menu.clients({ width=250 })
-      end
-    end
-  ),
-  awful.button({ }, 4,
-    function ()
-      awful.client.focus.byidx(1)
-      if client.focus then client.focus:raise() end
-    end
-  ),
-  awful.button({ }, 5,
-    function ()
-      awful.client.focus.byidx(-1)
-      if client.focus then client.focus:raise() end
-    end
-  )
-)
 
 for s = 1, screen.count() do
   -- Create a promptbox for each screen
@@ -74,9 +34,10 @@ for s = 1, screen.count() do
   mytaglist[s] = awful.widget.taglist(s, awful.widget.taglist.label.all, mytaglist.buttons)
 
   -- Create a tasklist widget
+  local mytasklist = {}
   mytasklist[s] = awful.widget.tasklist(function(c)
     return awful.widget.tasklist.label.currenttags(c, s)
-  end, mytasklist.buttons)
+  end)
 
   -- Create the wibox
   mywibox[s] = awful.wibox({ position = "top", screen = s })
