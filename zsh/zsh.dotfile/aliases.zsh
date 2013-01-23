ddc80e641e6c8aaae9a0cbd8472dec922fc9ba86
Andreas Launila <andreas.launila@magine.com>
2013-01-23 09:34:14 +0100

    Add alias for using custom keyboard layout

--- zsh/zsh.dotfile/aliases.zsh
+++ zsh/zsh.dotfile/aliases.zsh
@@ -40,6 +40,7 @@ alias so='source ~/.zshrc'
 
 # keyboard
 alias qwerty='setxkbmap se'
+alias custom='xkbcomp -I$HOME/.xkb $HOME/.xkb/keymap/custom "${DISPLAY%%.*}" -w 0'
 
 # Override prezto, don't ask when removing files
 alias rm='rm'
