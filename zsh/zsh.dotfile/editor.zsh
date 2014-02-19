396e19a55ab4b3754e47a4e915854e044533fd7d
Andreas Launila <andreas.launila@magine.com>
2014-02-19 15:26:04 +0100

    Add shortcut for prepending sudo

--- zsh/zsh.dotfile/editor.zsh
+++ zsh/zsh.dotfile/editor.zsh
@@ -1,1 +1,4 @@
 bindkey -M viins "$key_info[Control]R" history-incremental-pattern-search-backward
+
+insert_sudo () { zle beginning-of-line; zle -U "sudo " }
+bindkey -M viins "$key_info[Control]s" insert-sudo
