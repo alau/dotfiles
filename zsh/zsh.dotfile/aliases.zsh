b74f147bf8e154a7a69dbc6e0b8bfbb4901e1840
Andreas Launila <andreas.launila@magine.com>
2014-10-09 16:44:01 +0200

    Add amazon cli

--- zsh/zsh.dotfile/aliases.zsh
+++ zsh/zsh.dotfile/aliases.zsh
@@ -77,6 +77,7 @@ alias dotup='cd ~/dotfiles/ && gff && ./install.sh'
 alias follow='less +F'
 alias avro="java -jar $HOME/Dropbox/utils/avro-tools-1.7.7.jar "$@""
 alias http="$HOME/Dropbox/utils/python/bin/http "$@""
+alias aws="$HOME/Dropbox/bin/aws "$@""
 alias staging="MAGINE_ENV=staging sbt"
 alias sum="awk '{ sum += \$1 } END { print sum }' "$@""
 function zebra { awk 'NR%2 == 1 { printf("\033[48;5;237m%s\033[0m\n", $0); next}; 1'; }
