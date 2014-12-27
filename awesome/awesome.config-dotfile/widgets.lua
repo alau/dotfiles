5f9cb85b44096dc37325a760b0f419046998f5d6
Andreas Launila <github@lokorin.org>
2014-12-27 12:26:54 +0100

    Update widget declarations

--- awesome/awesome.config-dotfile/widgets.lua
+++ awesome/awesome.config-dotfile/widgets.lua
@@ -1,8 +1,10 @@
+wibox = require("wibox")
+
 -- Create a textclock widget
 local mytextclock = awful.widget.textclock()
 
 -- Create a systray
-local mysystray = widget({ type = "systray" })
+local mysystray = wibox.widget.systray()
 
 -- Create a wibox for each screen and add it
 local mywibox = {}
@@ -20,7 +22,7 @@ mytaglist.buttons = awful.util.table.join(
 
 for s = 1, screen.count() do
   -- Create a promptbox for each screen
-  mypromptbox[s] = awful.widget.prompt({ layout = awful.widget.layout.horizontal.leftright })
+  mypromptbox[s] = awful.widget.prompt({ layout = wibox.layout.align.horizontal })
   -- Create an imagebox widget which will contains an icon indicating which layout we're using.
   -- We need one layoutbox per screen.
   mylayoutbox[s] = awful.widget.layoutbox(s)
@@ -39,19 +41,22 @@ for s = 1, screen.count() do
 
   -- Create the wibox
   mywibox[s] = awful.wibox({ position = "top", screen = s })
-  -- Add widgets to the wibox - order matters
-  mywibox[s].widgets = {
-    {
-      mytaglist[s],
-      mypromptbox[s],
-      layout = awful.widget.layout.horizontal.leftright
-    },
-    mylayoutbox[s],
-    mytextclock,
-    s == 1 and mysystray or nil,
-    mytasklist[s],
-    layout = awful.widget.layout.horizontal.rightleft
-  }
+
+  local left_layout = wibox.layout.fixed.horizontal()
+  left_layout:add(mytaglist[s])
+  left_layout:add(mypromptbox[s])
+
+  local right_layout = wibox.layout.fixed.horizontal()
+  right_layout:add(mylayoutbox[s])
+  right_layout:add(mytextclock)
+  if s == 1 then right_layout:add(mysystray) end
+
+  local layout = wibox.layout.align.horizontal()
+  layout:set_left(left_layout)
+  layout:set_middle(mytasklist[s])
+  layout:set_right(right_layout)
+
+  mywibox[s]:set_widget(layout)
 end
 
 widgets.mypromptbox = mypromptbox
