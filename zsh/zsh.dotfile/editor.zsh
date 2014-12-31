c0b051aeffa0e827da031c04a524bcf8dd3d5313
Andreas Launila <github@lokorin.org>
2014-12-31 01:39:21 +0100

    Fix sudo-binding

--- zsh/zsh.dotfile/editor.zsh
+++ zsh/zsh.dotfile/editor.zsh
@@ -1,4 +1,5 @@
 bindkey -M viins "$key_info[Control]R" history-incremental-pattern-search-backward
 
-insert_sudo () { zle beginning-of-line; zle -U "sudo " }
+insert-sudo () { zle beginning-of-line; zle -U "sudo " }
+zle -N insert-sudo
 bindkey -M viins "$key_info[Control]s" insert-sudo
