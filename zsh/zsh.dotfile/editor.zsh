e69beb9bdd92734d9357e719af6b7972308ab745
Andreas Launila <github@lokorin.org>
2015-02-02 21:56:10 +0100

    Add some zsh shortcuts that I have been missing

--- zsh/zsh.dotfile/editor.zsh
+++ zsh/zsh.dotfile/editor.zsh
@@ -3,3 +3,8 @@ bindkey -M viins "$key_info[Control]R" history-incremental-pattern-search-backwa
 insert-sudo () { zle beginning-of-line; zle -U "sudo " }
 zle -N insert-sudo
 bindkey -M viins "$key_info[Control]s" insert-sudo
+
+bindkey "^W" backward-kill-word
+bindkey "^U" backward-kill-line
+
+bindkey -M vicmd 'yy' vi-yank-whole-line
