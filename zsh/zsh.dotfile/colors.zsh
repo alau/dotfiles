4342cd7c7775878c07f9c6d660fd660c5bee46f3
Andreas Launila <github@lokorin.org>
2015-02-03 20:00:27 +0100

    Improve color of grep matches
    They were difficult to read before.

--- zsh/zsh.dotfile/colors.zsh
+++ zsh/zsh.dotfile/colors.zsh
@@ -1,2 +1,5 @@
 # Color partial completions
 zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)(?)*==34=00}:${(s.:.)LS_COLORS}")';
+
+# colors for grep matches
+export GREP_COLORS='mt=7'
