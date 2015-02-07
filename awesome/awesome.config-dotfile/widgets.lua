4dba25c59552befcc5cb2eef6acb3f9bab901d04
Andreas Launila <github@lokorin.org>
2015-02-07 20:07:33 +0100

    Add widget showing memory usage

--- awesome/awesome.config-dotfile/widgets.lua
+++ awesome/awesome.config-dotfile/widgets.lua
@@ -36,11 +36,23 @@ cpuwidget:set_width(50)
 cpuwidget:set_background_color("#494B4F")
 cpuwidget:set_color({
   type = "linear",
-  from = { 0, 0 },
-  to = { 10, 0 },
-  stops = { {0, "#FF5656"}, {0.5, "#88A175"}, {1, "#AECF96" }}
+  from = {0, 0},
+  to = {10, 0},
+  stops = {{0, "#FF5656"}, {0.5, "#88A175"}, {1, "#AECF96"}}
 })
 vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
+
+-- Memory usage
+memwidget = awful.widget.graph()
+memwidget:set_width(50)
+memwidget:set_background_color("#494B4F")
+memwidget:set_color({
+  type = "linear",
+  from = {0, 0},
+  to = {10, 0},
+  stops = {{0, "#FF5656"}, {0.5, "#88A175"}, {1, "#AECF96"}}
+})
+vicious.register(memwidget, vicious.widgets.mem, "$1")
 
 -- Wifi link
 wifiwidget = wibox.widget.textbox()
@@ -93,6 +105,7 @@ for s = 1, screen.count() do
   right_layout:add(netwidget)
   right_layout:add(wifiwidget)
   right_layout:add(cpuwidget)
+  right_layout:add(memwidget)
   right_layout:add(mytextclock)
   right_layout:add(mylayoutbox[s])
   if s == 1 then right_layout:add(mysystray) end
