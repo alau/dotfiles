b720342eb318ac70e932280e6e251cb5520b3b01
Andreas Launila <github@lokorin.org>
2015-02-07 19:38:27 +0100

    Add widget for cpu-usage
    Nicked from http://awesome.naquadah.org/wiki/Vicious#CPU_usage_.28graph.29

--- awesome/awesome.config-dotfile/widgets.lua
+++ awesome/awesome.config-dotfile/widgets.lua
@@ -30,6 +30,18 @@ vicious.register(
   3
 )
 
+-- CPU usage
+cpuwidget = awful.widget.graph()
+cpuwidget:set_width(50)
+cpuwidget:set_background_color("#494B4F")
+cpuwidget:set_color({
+  type = "linear",
+  from = { 0, 0 },
+  to = { 10, 0 },
+  stops = { {0, "#FF5656"}, {0.5, "#88A175"}, {1, "#AECF96" }}
+})
+vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
+
 -- Systray
 local mysystray = wibox.widget.systray()
 
@@ -75,6 +87,7 @@ for s = 1, screen.count() do
 
   local right_layout = wibox.layout.fixed.horizontal()
   right_layout:add(netwidget)
+  right_layout:add(cpuwidget)
   right_layout:add(mytextclock)
   right_layout:add(mylayoutbox[s])
   if s == 1 then right_layout:add(mysystray) end
