372baad00a786a8e71b719b945b03345af26252c
Andreas Launila <andreas.launila@expressen.se>
2016-07-04 17:02:44 +0200

    Add alias for npm run

--- zsh/zsh.dotfile/aliases.zsh
+++ zsh/zsh.dotfile/aliases.zsh
@@ -76,7 +76,8 @@ fi
 function anaconda-deactivate { path=("${(@)path:#$HOME/anaconda*/bin}") }
 function anaconda-activate { path=("/home/alau/anaconda2/bin" "/home/alau/anaconda3/bin" $path) }
 
-# mocha
+# node
+alias nr='npm run'
 alias mwa='mocha --watch'
 
 # Disabled corrections
