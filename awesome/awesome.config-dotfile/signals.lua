a6a53c94877f0a02ab5f9c3012aa220787a5383a
Andreas Launila <andreas.launila@expressen.se>
2016-07-27 09:06:33 +0200

    Do not auto-focus slack

--- awesome/awesome.config-dotfile/signals.lua
+++ awesome/awesome.config-dotfile/signals.lua
@@ -5,7 +5,7 @@ client.connect_signal("manage",
     c:connect_signal("mouse::enter",
       function(c)
         if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
-          and awful.client.focus.filter(c) and not (c.icon_name and c.icon_name:find("HipChat")) then
+          and awful.client.focus.filter(c) and not (c.class and c.class:find("Slack")) then
           client.focus = c
         end
       end
