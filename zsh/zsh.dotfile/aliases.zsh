30d2abdb2d57ce5c92e96388dee1b607c1ac4617
Andreas Launila <andreas.launila@expressen.se>
2018-01-18 09:18:49 +0100

    Add alias
    I think I had this a symlink before. Fits better as an alias.

--- zsh/zsh.dotfile/aliases.zsh
+++ zsh/zsh.dotfile/aliases.zsh
@@ -94,6 +94,7 @@ alias eslint='nocorrect eslint'
 # Misc
 alias dotup='cd ~/dotfiles/ && gff && ./install.sh'
 alias follow='less +F'
+alias top='htop'
 alias sum="awk '{ sum += \$1 } END { print sum }' "$@""
 function zebra { awk 'NR%2 == 1 { printf("\033[48;5;237m%s\033[0m\n", $0); next}; 1'; }
 function csv { sed 's/,/, /g' "$@" | column -s, -t | zebra | less -S }
