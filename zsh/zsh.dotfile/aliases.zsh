206347e0e763243f3ec610858a89c5c9f04a86ac
Andreas Launila <andreas.launila@expressen.se>
2018-02-22 09:23:36 +0100

    Add an alias for bringing up sound controls

--- zsh/zsh.dotfile/aliases.zsh
+++ zsh/zsh.dotfile/aliases.zsh
@@ -100,4 +100,5 @@ function zebra { awk 'NR%2 == 1 { printf("\033[48;5;237m%s\033[0m\n", $0); next}
 function csv { sed 's/,/, /g' "$@" | column -s, -t | zebra | less -S }
 function csv2 { csv2sc "$@" | sc }
 alias avro=avro-tools
+alias hdmi-sound=pavucontrol
 timestamp() { date -d @"$@" }
