146bfe3983791d96a5e56e37cf37e9801d8beee4
Andreas Launila <andreas.launila@expressen.se>
2019-10-23 15:46:13 +0200

    Add time and vi mode

--- zsh/zsh.dotfile/prompt.zsh
+++ zsh/zsh.dotfile/prompt.zsh
@@ -2,6 +2,6 @@ POWERLEVEL9K_PROMPT_ON_NEWLINE=true
 POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
 POWERLEVEL9K_MODE='nerdfont-fontconfig'
 
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(root_indicator dir nvm vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status zsh_showStatus)
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(root_indicator dir pyenv nvm vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status zsh_showStatus vi_mode time)
 
