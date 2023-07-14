64e1576940cd9124bbc9fbf6efcb0dae28170837
Andreas Launila <github@lokorin.org>
2023-07-14 21:02:09 +0200

    Tweak lsp config

--- neovim/nvim.config-dotfile/lua/alau/plugin_configs/lsp.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugin_configs/lsp.lua
@@ -1,16 +1,22 @@
+vim.opt.updatetime = 300
+-- Always show the signcolumn to avoid shifting text when it appears
+vim.opt.signcolumn = "yes"
+
 require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = { "lua_ls", "pyright" }
 })
 
-local on_attach = function()
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+local on_attach = function(_, bufnr)
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
