7ab6418f9e366afdb40c8cf88bf1130f34ca6b36
Andreas Launila <github@lokorin.org>
2023-07-15 13:08:17 +0200

    Fix airline symbols

--- neovim/nvim.config-dotfile/lua/alau/plugin_configs/airline.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugin_configs/airline.lua
@@ -1,10 +1,3 @@
-if vim.fn.exists('airline_symbols') ~= 0 then
-  vim.g.airline_symbols = {}
-end
-vim.g.airline_left_sep = '⮀'
-vim.g.airline_left_alt_sep = '⮁'
-vim.g.airline_right_sep = '⮂'
-vim.g.airline_right_alt_sep = '⮃'
-vim.g.airline_symbols = {branch = '⭠', readonly = '⭤'}
+vim.g.airline_powerline_fonts = 1
 vim.g['airline#extensions#branch#format'] = 2
 
