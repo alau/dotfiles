2f7812f9cb7a7988acbdeb6f7cfc95524a80ff72
Andreas Launila <github@lokorin.org>
2023-07-17 17:09:12 +0200

    Add mapping for fugutive

--- neovim/nvim.config-dotfile/lua/alau/plugin_configs/fugutive.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugin_configs/fugutive.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("n", "<C-g>", ":Git<CR>)")
+vim.keymap.set("n", "<C-c>", ":Glcd<CR>)")
 vim.keymap.set("", "<Leader>d", ":Git diff %<CR>")
 vim.keymap.set("", "<Leader>D", ":Git diff<CR>")
 
