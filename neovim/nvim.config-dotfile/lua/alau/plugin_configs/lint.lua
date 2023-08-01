7c67e22d20b1551dc7e8039a8fdb8b150d544679
Andreas Launila <andreas@lokorin.org>
2023-08-01 16:06:06 +0200

    Use the activated virtualenv when loading ruff

--- neovim/nvim.config-dotfile/lua/alau/plugin_configs/lint.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugin_configs/lint.lua
@@ -2,4 +2,19 @@ require('lint').linters_by_ft = {
   python = { 'ruff', 'mypy', }
 }
 
+local util = require('lspconfig/util')
+local path = util.path
+local function get_ruff_path(workspace)
+  -- Use activated virtualenv.
+  if vim.env.VIRTUAL_ENV then
+    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'ruff')
+  end
+
+  return 'ruff'
+end
+
+require('lint').linters.ruff.env = {
+  cmd = get_ruff_path(vim.fn.getcwd())
+}
+
 vim.cmd [[au BufWritePost * lua require('lint').try_lint()]]
