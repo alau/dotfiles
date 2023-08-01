84750f3dd7626688296575d9d7d1c6fc28ce46d9
Andreas Launila <andreas.launila@budbee.com>
2023-08-01 13:20:52 +0200

    Tweak navigation

--- neovim/nvim.config-dotfile/lua/alau/mappings.lua
+++ neovim/nvim.config-dotfile/lua/alau/mappings.lua
@@ -15,9 +15,13 @@ vim.keymap.set("n", "]s", ":cnext<CR>")
 vim.keymap.set("n", "<C-s>", ":write<CR>")
 vim.keymap.set("i", "<C-s>", "<Esc>:write<CR>a")
 
--- Move
+-- Move code
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Navigation
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- ctags
 vim.keymap.set("", "<Leader>rt", ":!ctags -R *<CR><CR>")
