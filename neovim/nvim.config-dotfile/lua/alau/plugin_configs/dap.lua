77052b3ecaef47028e95b46fb5d56bea83bc67f3
Andreas Launila <andreas.launila@budbee.com>
2023-08-03 13:01:22 +0200

    Tweak mappings

--- neovim/nvim.config-dotfile/lua/alau/plugin_configs/dap.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugin_configs/dap.lua
@@ -15,8 +15,8 @@ end
 
 vim.keymap.set('n', '<F5>', dap.continue)
 vim.keymap.set('n', '<F8>', dap.toggle_breakpoint)
-vim.keymap.set('n', '<F10>', dap.step_over)
-vim.keymap.set('n', '<F11>', dap.step_into)
+vim.keymap.set('n', '<F10>', dap.step_into)
+vim.keymap.set('n', '<F11>', dap.step_over)
 vim.keymap.set('n', '<F12>', dap.step_out)
 
 -- Language support
