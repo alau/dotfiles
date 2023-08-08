2cc328a6359a198e72516dd91adfa2848ceb3184
Andreas Launila <andreas.launila@budbee.com>
2023-08-08 12:22:02 +0200

    Pick the mypy from the current virual env

--- neovim/nvim.config-dotfile/lua/alau/plugin_configs/lint.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugin_configs/lint.lua
@@ -2,18 +2,22 @@ require('lint').linters_by_ft = {
   python = { 'ruff', 'mypy', }
 }
 
-local function get_ruff_path(_)
+local function get_cmd(executable)
   local util = require('lspconfig/util')
   local path = util.path
   if vim.env.VIRTUAL_ENV then
-    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'ruff')
+    return path.join(vim.env.VIRTUAL_ENV, 'bin', executable)
   end
 
-  return 'ruff'
+  return executable
 end
 
 require('lint').linters.ruff.env = {
-  cmd = get_ruff_path(vim.fn.getcwd())
+  cmd = get_cmd("ruff")
+}
+
+require('lint').linters.mypy.env = {
+  cmd = get_cmd("mypy")
 }
 
 vim.cmd [[au BufWritePost * silent! lua require('lint').try_lint()]]
