ad4eddd2a750611b2467113b7b0d954c616ef1fe
Andreas Launila <andreas.launila@budbee.com>
2023-08-11 10:08:54 +0200

    Add trouble plugin

--- neovim/nvim.config-dotfile/lua/alau/plugins.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugins.lua
@@ -22,6 +22,9 @@ return require('packer').startup({function(use)
   use 'mhinz/vim-startify'
   use 'simrat39/symbols-outline.nvim'
   use 'preservim/vimux'
+  use 'nvim-neotest/neotest'
+  use 'nvim-neotest/neotest-python'
+  use 'folke/trouble.nvim'
 
   -- Utility
   use 'tpope/vim-sensible'
@@ -40,8 +43,6 @@ return require('packer').startup({function(use)
   }
   use 'nvim-treesitter/nvim-treesitter-textobjects'
   use 'nvim-treesitter/nvim-treesitter-context'
-  use 'nvim-neotest/neotest'
-  use 'nvim-neotest/neotest-python'
   use 'antoinemadec/FixCursorHold.nvim'
   use 'Almo7aya/openingh.nvim'
   use 'dense-analysis/ale'
