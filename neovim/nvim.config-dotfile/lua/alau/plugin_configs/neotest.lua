764fea891409941f3e57efb5734d44a809057dda
Andreas Launila <andreas.launila@budbee.com>
2023-08-11 13:45:39 +0200

    Add mapping for aborting a test

--- neovim/nvim.config-dotfile/lua/alau/plugin_configs/neotest.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugin_configs/neotest.lua
@@ -18,6 +18,7 @@ require("neotest").setup({
 })
 
 vim.keymap.set("n", "<Leader>rt", ':lua require("neotest").run.run()<CR>')
+vim.keymap.set("n", "<Leader>ra", ':lua require("neotest").run.stop()<CR>')
 vim.keymap.set("n", "<Leader>rd", ':lua require("neotest").run.run({strategy = "dap"})<CR>')
 vim.keymap.set("n", "<Leader>rr", ':lua require("neotest").run.run_last()<CR>')
 vim.keymap.set("n", "<Leader>rf", ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>')
