31b8415a9b07072af431810b58bda5c03c436495
Andreas Launila <andreas.launila@budbee.com>
2023-08-15 13:03:42 +0200

    Apply whitespace fixing regardless of language

--- neovim/nvim.config-dotfile/lua/alau/plugin_configs/ale.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugin_configs/ale.lua
@@ -2,7 +2,10 @@ vim.g.ale_use_neovim_diagnostics_api = 1
 
 vim.g.ale_linters = { python = { 'ruff', 'mypy' } }
 
-vim.g.ale_fixers = { python = { 'black', 'trim_whitespace', 'autoimport', 'isort' } }
+vim.g.ale_fixers = {
+  python = { 'trim_whitespace', 'black', 'autoimport', 'isort' },
+  ['*'] = { 'trim_whitespace' }
+}
 vim.g.ale_fix_on_save = 1
 
 vim.api.nvim_set_keymap('n', '<Leader>i', ':lua vim.diagnostic.open_float()<CR>', {})
