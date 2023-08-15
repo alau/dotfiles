b8069fa1073766a6478982544020a62fbfc31322
Andreas Launila <andreas.launila@budbee.com>
2023-08-15 12:48:20 +0200

    Enable folding with treesitter

--- neovim/nvim.config-dotfile/lua/alau/plugin_configs/treesitter.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugin_configs/treesitter.lua
@@ -1,3 +1,6 @@
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+
 require('nvim-treesitter.configs').setup({
   ensure_installed = { "python", "lua", "json", "sql", "javascript", "rust", "yaml", "dockerfile" },
 
