51ade9984ffd8d0d2488021842442b8aa65d85d6
Andreas Launila <andreas.launila@budbee.com>
2023-08-28 10:49:05 +0200

    Add tool for ripgrep

--- neovim/nvim.config-dotfile/lua/alau/plugins.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugins.lua
@@ -26,6 +26,7 @@ return require('packer').startup({function(use)
   use 'nvim-neotest/neotest-python'
   use 'folke/zen-mode.nvim'
   use 'ThePrimeagen/harpoon'
+  use 'duane9/nvim-rg'
 
   -- Utility
   use 'tpope/vim-sensible'
