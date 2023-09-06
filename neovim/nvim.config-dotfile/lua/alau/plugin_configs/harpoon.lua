e5a06625c6da7f130f9f66ce2e3cef4615507d7d
Andreas Launila <andreas.launila@budbee.com>
2023-09-06 12:08:01 +0200

    Tweak mapping for harpoon

--- neovim/nvim.config-dotfile/lua/alau/plugin_configs/harpoon.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugin_configs/harpoon.lua
@@ -1,7 +1,5 @@
 vim.keymap.set("n", "<Leader>ha", require("harpoon.mark").add_file)
 vim.keymap.set("n", "<Leader>hq", require("harpoon.ui").toggle_quick_menu)
-vim.keymap.set("n", "1", ':lua require("harpoon.ui").nav_file(1)<CR>zz')
-vim.keymap.set("n", "2", ':lua require("harpoon.ui").nav_file(2)<CR>zz')
-vim.keymap.set("n", "3", ':lua require("harpoon.ui").nav_file(3)<CR>zz')
-vim.keymap.set("n", "4", ':lua require("harpoon.ui").nav_file(4)<CR>zz')
+vim.keymap.set("n", "(", ':lua require("harpoon.ui").nav_file(1)<CR>zz')
+vim.keymap.set("n", ")", ':lua require("harpoon.ui").nav_file(2)<CR>zz')
 
