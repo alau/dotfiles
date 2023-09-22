85706de0191c24e1a2de958ff929ab237595ec11
Andreas Launila <andreas.launila@budbee.com>
2023-09-22 11:20:51 +0200

    Configure ALE for js

--- neovim/nvim.config-dotfile/lua/alau/plugin_configs/ale.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugin_configs/ale.lua
@@ -3,6 +3,7 @@ vim.g.ale_use_neovim_diagnostics_api = 1
 vim.g.ale_linters = {
   python = { 'ruff', 'mypy' },
   yaml = { 'yamllint' },
+  javascript = { 'eslint' },
 }
 vim.g.ale_python_mypy_auto_poetry = 1
 vim.g.ale_python_mypy_auto_pipenv = 1
@@ -10,6 +11,7 @@ vim.g.ale_python_mypy_options = '--follow-imports=silent'
 
 vim.g.ale_fixers = {
   python = { 'trim_whitespace', 'black', 'autoimport', 'isort' },
+  javascript = { 'trim_whitespace', 'prettier' },
   ['*'] = { 'trim_whitespace' }
 }
 vim.g.ale_fix_on_save = 1
