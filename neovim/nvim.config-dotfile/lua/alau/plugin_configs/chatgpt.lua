e8526fcaead398d90e404375742f869b99eb1f46
Andreas Launila <andreas.launila@instabee.com>
2023-12-06 12:06:29 +0100

    Increase tokens

--- neovim/nvim.config-dotfile/lua/alau/plugin_configs/chatgpt.lua
+++ neovim/nvim.config-dotfile/lua/alau/plugin_configs/chatgpt.lua
@@ -3,7 +3,7 @@ require("chatgpt").setup({
     model = "gpt-4-1106-preview",
     frequency_penalty = 0,
     presence_penalty = 0,
-    max_tokens = 300,
+    max_tokens = 500,
     temperature = 0,
     top_p = 1,
     n = 1,
