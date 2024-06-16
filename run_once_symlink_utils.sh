f257005a0c8910c2977fb8855aa5a727f9bd8160
Andreas Launila <github@lokorin.org>
2024-06-16 19:01:15 +0200

    Remove dummy symlink

--- run_once_symlink_utils.sh
+++ run_once_symlink_utils.sh
@@ -2,7 +2,7 @@
 #
 # Symlink various utilities from Dropbox
 mkdir -p $HOME/bin
-for util in 'avro-tools' 'foo' 'csv2sc' 'nvfancontrol'; do
+for util in 'avro-tools' 'csv2sc' 'nvfancontrol'; do
   if ! [[ -L $HOME/bin/$util ]]; then
     ln -s -v -n $HOME/Dropbox/bin/$util $HOME/bin/$util
   fi
