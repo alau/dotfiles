c349ab04f866b61455892db11aed264e71450415
Andreas Launila <andreas.launila@instabee.com>
2024-08-27 08:28:22 +0200

    Switch to luaformatter

--- dot_config/nvim/lua/alau/plugin_configs/conform.lua
+++ dot_config/nvim/lua/alau/plugin_configs/conform.lua
@@ -1,13 +1,10 @@
 require("conform").setup({
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_format = "fallback",
-  },
-  formatters_by_ft = {
-    ['*'] = { 'trim_whitespace' },
-    python = { 'black', 'autoimport', 'isort' },
-    go = { 'golines' },
-    javascript = { 'prettier' },
-    lua = { "stylua" },
-  },
+    format_on_save = {timeout_ms = 500, lsp_format = "fallback"},
+    formatters_by_ft = {
+        ["*"] = {"trim_whitespace"},
+        python = {"black", "autoimport", "isort"},
+        go = {"golines"},
+        javascript = {"prettier"},
+        lua = {"lua-format"}
+    }
 })
