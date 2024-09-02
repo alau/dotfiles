20579d47fb4c3e23f4e3bc29c79c0f92603cb0e6
Andreas Launila <andreas.launila@instabee.com>
2024-09-02 14:31:56 +0200

    Fix linter config

--- dot_config/nvim/lua/alau/plugin_configs/nvim_lint.lua
+++ dot_config/nvim/lua/alau/plugin_configs/nvim_lint.lua
@@ -8,8 +8,8 @@ require("lint").linters_by_ft = {
     dockerfile = {"hadolint"}
 }
 
-table.insert(require('lint').linters.mypy.args, 0, '--follow-imports=silent')
-table.insert(require('lint').linters.yamllint.args, 0,
+table.insert(require('lint').linters.mypy.args, 1, '--follow-imports=silent')
+table.insert(require('lint').linters.yamllint.args, 1,
              '-d "{extends: default, rules: {line-length: {max: 120}, truthy: disable, document-start: disable}}"')
 
 vim.api.nvim_create_autocmd({"BufWritePost"}, {
