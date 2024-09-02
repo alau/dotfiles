83019bfca123df74c84f3cc4cfa28fae9f0e37f2
Andreas Launila <andreas.launila@instabee.com>
2024-09-02 14:06:56 +0200

    Replace autoimport with ruff_fix
    Autoimport is not in the list of conform's formatters, and ruff_fix
    covers what I need (imports are covered by LSP).

--- dot_config/nvim/lua/alau/plugin_configs/conform.lua
+++ dot_config/nvim/lua/alau/plugin_configs/conform.lua
@@ -2,7 +2,7 @@ require("conform").setup({
     format_on_save = {timeout_ms = 500, lsp_format = "fallback"},
     formatters_by_ft = {
         ["*"] = {"trim_whitespace"},
-        python = {"black", "autoimport", "isort"},
+        python = {"ruff_fix", "black", "isort"},
         go = {"golines"},
         javascript = {"prettier"},
         lua = {"lua-format"}
