e0aa3ef0a3bcf736f0d190c8fda38ab44c869c75
Andreas Launila <andreas.launila@instabee.com>
2024-09-18 09:37:01 +0200

    Disregard preselcted completions from lang server
    It was making the completion in Go wonky due to gopls preselcting
    certain options.
    
    https://www.reddit.com/r/neovim/comments/1139ngo/comment/j8qnoh3/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button

--- dot_config/nvim/lua/alau/completions.lua
+++ dot_config/nvim/lua/alau/completions.lua
@@ -62,5 +62,6 @@ cmp.setup({
     sources = cmp.config.sources({
         {name = 'nvim_lsp'}, {name = 'luasnip'},
         {name = 'nvim_lsp_signature_help'}
-    }, {{name = 'buffer'}})
+    }, {{name = 'buffer'}}),
+    preselect = cmp.PreselectMode.None
 })
