ad44e3caa66ecb5efdcabd9240f80d410c7fa82a
Andreas Launila <andreas.launila@instabee.com>
2025-01-07 11:13:49 +0100

    Apply formatter

--- dot_config/nvim/lua/alau/plugin_configs/flash.lua
+++ dot_config/nvim/lua/alau/plugin_configs/flash.lua
@@ -1,33 +1,18 @@
 require('flash').setup({
-  labels = 'hutenosadimkwjvqzbxgpcrlf',
-  modes = {
-    char = {
-      keys = { "f", "F" },
-      jump_labels = true
+    labels = 'hutenosadimkwjvqzbxgpcrlf',
+    modes = {
+        char = {keys = {"f", "F"}, jump_labels = true},
+        search = {enabled = true}
     },
-    search = {
-      enabled = true
-    }
-  },
-  label = {
-    rainbow = {
-      enabled = true
-    }
-  }
+    label = {rainbow = {enabled = true}}
 })
 
-vim.keymap.set({ "n", "x", "o" }, "F", function()
-  require("flash").jump()
-end)
+vim.keymap.set({"n", "x", "o"}, "F", function() require("flash").jump() end)
 
-vim.keymap.set({ "n", "x", "o" }, "S", function()
-  require("flash").treesitter()
-end)
+vim.keymap.set({"n", "x", "o"}, "S",
+               function() require("flash").treesitter() end)
 
-vim.keymap.set("o", "r", function()
-  require("flash").remote()
-end)
+vim.keymap.set("o", "r", function() require("flash").remote() end)
 
-vim.keymap.set({ "o", "x" }, "R", function()
-  require("flash").treesitter_search()
-end)
+vim.keymap.set({"o", "x"}, "R",
+               function() require("flash").treesitter_search() end)
