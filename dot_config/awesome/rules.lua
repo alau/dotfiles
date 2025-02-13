0015eb30933986455b224ca390f227347663a223
Andreas Launila <andreas.launila@instabee.com>
2025-02-13 12:18:53 +0100

    Formatting

--- dot_config/awesome/rules.lua
+++ dot_config/awesome/rules.lua
@@ -1,32 +1,24 @@
 rules.rules = {
-  {
-    -- All clients will match this rule.
-    rule = { },
-    properties = {
-      border_width = beautiful.border_width,
-      border_color = beautiful.border_normal,
-      focus = true,
-      keys = settings.clientkeys,
-      maximized_vertical = false,
-      maximized_horizontal = false,
-      size_hints_honor = false,
-      buttons = settings.clientbuttons,
-    },
-    callback = function(c)
-      c.screen = mouse.screen
-    end
-  },
-  {
-    rule = { class = "MPlayer" },
-    properties = { floating = true }
-  },
-  {
-    rule = { instance = "plugin-container" },
-    properties = { floating = true, focus = yes }
-  },
+    {
+        -- All clients will match this rule.
+        rule = {},
+        properties = {
+            border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus = true,
+            keys = settings.clientkeys,
+            maximized_vertical = false,
+            maximized_horizontal = false,
+            size_hints_honor = false,
+            buttons = settings.clientbuttons
+        },
+        callback = function(c) c.screen = mouse.screen end
+    }, {rule = {class = "MPlayer"}, properties = {floating = true}}, {
+        rule = {instance = "plugin-container"},
+        properties = {floating = true, focus = yes}
+    }
 }
 
-local local_additions = os.getenv("HOME") .. "/.awesome-additional-rules.local.lua"
-if awful.util.file_readable(local_additions) then
-  dofile(local_additions)
-end
+local local_additions = os.getenv("HOME") ..
+                            "/.awesome-additional-rules.local.lua"
+if awful.util.file_readable(local_additions) then dofile(local_additions) end
