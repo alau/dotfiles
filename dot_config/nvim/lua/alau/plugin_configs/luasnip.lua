e50c438dec3f16ae3ff8f16a0975e1e395810667
Andreas Launila <andreas.launila@instabee.com>
2025-03-18 13:27:46 +0100

    Add snippet for adding a cell

--- dot_config/nvim/lua/alau/plugin_configs/luasnip.lua
+++ dot_config/nvim/lua/alau/plugin_configs/luasnip.lua
@@ -4,3 +4,11 @@ require("luasnip.loaders.from_vscode").lazy_load()
 
 luasnip.config.set_config({store_selection_keys = '<C-A>'})
 
+local i = luasnip.insert_node
+local s = luasnip.snippet
+local t = luasnip.text_node
+luasnip.add_snippets("markdown", {
+    -- code cell
+    s({trig = "^%s*`", trigEngine = "pattern"},
+      {t({"```python", ""}), i(1), t({"", "```"})})
+})
