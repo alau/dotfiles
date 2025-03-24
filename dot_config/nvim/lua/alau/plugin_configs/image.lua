fe362715ae89ac41bd849d491f6355fb16c37c41
Andreas Launila <andreas.launila@instabee.com>
2025-03-24 10:53:10 +0100

    Avoid images being loaded for wrong tmux tab

--- dot_config/nvim/lua/alau/plugin_configs/image.lua
+++ dot_config/nvim/lua/alau/plugin_configs/image.lua
@@ -13,5 +13,6 @@ require("image").setup({
     max_height_window_percentage = math.huge,
     max_width_window_percentage = math.huge,
     window_overlap_clear_enabled = true,
-    window_overlap_clear_ft_ignore = {"cmp_menu", "cmp_docs", ""}
+    window_overlap_clear_ft_ignore = {"cmp_menu", "cmp_docs", ""},
+    editor_only_render_when_focused = true
 })
