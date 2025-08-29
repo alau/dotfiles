551f968e0182d611257bd25084b238c0c1d7d4a1
Andreas Launila <andreas.launila@instabee.com>
2025-08-29 16:25:01 +0200

    Switch back to gemini

--- dot_config/nvim/lua/alau/plugin_configs/copilot_chat.lua
+++ dot_config/nvim/lua/alau/plugin_configs/copilot_chat.lua
@@ -4,7 +4,7 @@ local select = require('CopilotChat.select')
 vim.g.copilot_enabled = false
 
 vim.opt.completeopt = vim.opt.completeopt + {'noinsert', 'noselect', 'popup'}
-chat.setup({model = 'claude-sonnet-4', mappings = {reset = {normal = '<C-r>', insert = '<C-r>'}}, window = {width = 80}})
+chat.setup({model = 'gemini-2.5-pro', mappings = {reset = {normal = '<C-r>', insert = '<C-r>'}}, window = {width = 80}})
 
 vim.keymap.set({'n'}, '<leader>co', function() chat.open({selection = select.buffer}) end)
 vim.keymap.set({'v'}, '<leader>co', function() chat.open({selection = select.visual}) end)
