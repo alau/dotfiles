df5282c0842085745817d1ec337a62d90b08d1a8
Andreas Launila <andreas.launila@instabee.com>
2025-09-03 05:29:15 +0200

    Increase timeout
    golines is running into the limit, so let's give it some more time.

--- dot_config/nvim/lua/alau/plugin_configs/conform.lua
+++ dot_config/nvim/lua/alau/plugin_configs/conform.lua
@@ -3,7 +3,7 @@ c.setup({
     format_on_save = function(bufnr)
         -- Disable with a global or buffer-local variable
         if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
-        return {timeout_ms = 1000, lsp_format = "fallback"}
+        return {timeout_ms = 2000, lsp_format = "fallback"}
     end,
     formatters_by_ft = {
         ["*"] = {"trim_whitespace"},
