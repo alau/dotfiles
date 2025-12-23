b71224d9fef76da1077023094df002077f1dfc13
Andreas Launila <andreas.launila@instabee.com>
2025-12-23 07:13:25 +0100

    Close immediately on yank

--- dot_config/nvim/lua/alau/plugin_configs/kitty_scrollback.lua
+++ dot_config/nvim/lua/alau/plugin_configs/kitty_scrollback.lua
@@ -1,1 +1,8 @@
-require('kitty-scrollback').setup()
+require('kitty-scrollback').setup({
+    ksb_builtin_get_text_all = {
+        callbacks = {
+            after_ready = function() vim.opt.clipboard = 'unnamedplus' end,
+            after_paste_window_ready = function(_, _, _) vim.cmd('qa!') end
+        }
+    }
+})
