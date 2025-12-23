9abd6e50131057cfafe67c4d6103dc297ae7ab11
Andreas Launila <andreas.launila@instabee.com>
2025-12-23 07:40:52 +0100

    Add mapping to copy output from last command

--- dot_config/nvim/lua/alau/plugin_configs/kitty_scrollback.lua
+++ dot_config/nvim/lua/alau/plugin_configs/kitty_scrollback.lua
@@ -1,8 +1,6 @@
 require('kitty-scrollback').setup({
-    ksb_builtin_get_text_all = {
-        callbacks = {
-            after_ready = function() vim.opt.clipboard = 'unnamedplus' end,
-            after_paste_window_ready = function(_, _, _) vim.cmd('qa!') end
-        }
+    get_text_all = {
+        kitty_get_text = {extent = 'all'},
+        callbacks = {after_paste_window_ready = function(_, _, _) vim.cmd('qa!') end}
     }
 })
