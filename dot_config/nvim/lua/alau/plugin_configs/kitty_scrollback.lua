9fa02825f9bda09fd3e41e518df2611ae96d4a25
Andreas Launila <andreas.launila@instabee.com>
2025-12-29 07:56:52 +0100

    Change the behavior of page up

--- dot_config/nvim/lua/alau/plugin_configs/kitty_scrollback.lua
+++ dot_config/nvim/lua/alau/plugin_configs/kitty_scrollback.lua
@@ -2,5 +2,13 @@ require('kitty-scrollback').setup({
     get_text_all = {
         kitty_get_text = {extent = 'all'},
         callbacks = {after_paste_window_ready = function(_, _, _) vim.cmd('qa!') end}
+    },
+    get_text_all_page_up = {
+        kitty_get_text = {extent = 'all'},
+        callbacks = {
+            after_ready = function()
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<PageUp>zz', true, false, true), 'n', false)
+            end
+        }
     }
 })
