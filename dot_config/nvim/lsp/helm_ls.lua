8803321b013c1148a51f979546ac6d1ca34588cb
Andreas Launila <andreas.launila@instabee.com>
2026-01-02 12:47:58 +0100

    Fix helm_ls config

--- dot_config/nvim/lsp/helm_ls.lua
+++ dot_config/nvim/lsp/helm_ls.lua
@@ -1,5 +1,1 @@
-return {
-    cmd = {"helm_ls", "-l", "/tmp/helm-ls.log"},
-    filetypes = {'helm', 'yaml'},
-    root_markers = {'Chart.yaml', '.git'}
-}
+return {cmd = {"helm_ls", "serve"}, filetypes = {'helm', 'yaml'}, root_markers = {'Chart.yaml', '.git'}}
