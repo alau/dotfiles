16d9a5adb0aeefe2559479941703b698dcedb931
Andreas Launila <andreas.launila@instabee.com>
2026-02-06 08:19:53 +0100

    Switch to gemini 3 as default
    The 3x multiplier for opus is a bit hefty. Let's just use it when
    needed.

--- dot_config/nvim/lua/alau/plugin_configs/copilot_chat.lua
+++ dot_config/nvim/lua/alau/plugin_configs/copilot_chat.lua
@@ -5,7 +5,7 @@ vim.g.copilot_enabled = false
 
 vim.opt.completeopt = vim.opt.completeopt + {'noinsert', 'noselect', 'popup'}
 chat.setup({
-    model = 'claude-opus-4.5',
+    model = 'gemini-3-pro-preview',
     sticky = {"#buffer:visible"},
     mappings = {reset = {normal = '<C-r>', insert = '<C-r>'}},
     window = {width = 80},
