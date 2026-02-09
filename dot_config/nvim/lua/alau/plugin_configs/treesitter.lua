58b7d6a07982a5a269b65d0770a89a041a36b839
Andreas Launila <andreas.launila@instabee.com>
2026-02-09 07:46:44 +0100

    Migrate textobjects config

--- dot_config/nvim/lua/alau/plugin_configs/treesitter.lua
+++ dot_config/nvim/lua/alau/plugin_configs/treesitter.lua
@@ -7,28 +7,38 @@ require('nvim-treesitter').install({
     "python", "lua", "json", "sql", "javascript", "yaml", "dockerfile", "go", "vimdoc", "diff", "markdown", "helm"
 })
 
-require("nvim-treesitter-textobjects").setup({
-    select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["ib"] = "@code_cell.inner",
-            ["ab"] = "@code_cell.outer"
-        }
-    },
-    move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = {[']f'] = '@function.outer', [']]'] = '@class.outer', ["]b"] = "@code_cell.inner"},
-        goto_next_end = {[']F'] = '@function.outer', [']['] = '@class.outer', ["]B"] = "@code_cell.inner"},
-        goto_previous_start = {['[f'] = '@function.outer', ['[['] = '@class.outer', ["[b"] = "@code_cell.inner"},
-        goto_previous_end = {['[F'] = '@function.outer', ['[]'] = '@class.outer', ["[B"] = "@code_cell.inner"}
-    }
-})
+require("nvim-treesitter-textobjects").setup({select = {lookahead = true}, move = {set_jumps = true}})
+
+local ts_select = require("nvim-treesitter-textobjects.select")
+local ts_move = require("nvim-treesitter-textobjects.move")
+
+-- Select textobjects
+vim.keymap.set({"x", "o"}, "af", function() ts_select.select_textobject("@function.outer", "textobjects") end)
+vim.keymap.set({"x", "o"}, "if", function() ts_select.select_textobject("@function.inner", "textobjects") end)
+vim.keymap.set({"x", "o"}, "ac", function() ts_select.select_textobject("@class.outer", "textobjects") end)
+vim.keymap.set({"x", "o"}, "ic", function() ts_select.select_textobject("@class.inner", "textobjects") end)
+vim.keymap.set({"x", "o"}, "ib", function() ts_select.select_textobject("@code_cell.inner", "textobjects") end)
+vim.keymap.set({"x", "o"}, "ab", function() ts_select.select_textobject("@code_cell.outer", "textobjects") end)
+
+-- Move textobjects - next start
+vim.keymap.set({"n", "x", "o"}, "]f", function() ts_move.goto_next_start("@function.outer", "textobjects") end)
+vim.keymap.set({"n", "x", "o"}, "]]", function() ts_move.goto_next_start("@class.outer", "textobjects") end)
+vim.keymap.set({"n", "x", "o"}, "]b", function() ts_move.goto_next_start("@code_cell.inner", "textobjects") end)
+
+-- Move textobjects - next end
+vim.keymap.set({"n", "x", "o"}, "]F", function() ts_move.goto_next_end("@function.outer", "textobjects") end)
+vim.keymap.set({"n", "x", "o"}, "][", function() ts_move.goto_next_end("@class.outer", "textobjects") end)
+vim.keymap.set({"n", "x", "o"}, "]B", function() ts_move.goto_next_end("@code_cell.inner", "textobjects") end)
+
+-- Move textobjects - previous start
+vim.keymap.set({"n", "x", "o"}, "[f", function() ts_move.goto_previous_start("@function.outer", "textobjects") end)
+vim.keymap.set({"n", "x", "o"}, "[[", function() ts_move.goto_previous_start("@class.outer", "textobjects") end)
+vim.keymap.set({"n", "x", "o"}, "[b", function() ts_move.goto_previous_start("@code_cell.inner", "textobjects") end)
+
+-- Move textobjects - previous end
+vim.keymap.set({"n", "x", "o"}, "[F", function() ts_move.goto_previous_end("@function.outer", "textobjects") end)
+vim.keymap.set({"n", "x", "o"}, "[]", function() ts_move.goto_previous_end("@class.outer", "textobjects") end)
+vim.keymap.set({"n", "x", "o"}, "[B", function() ts_move.goto_previous_end("@code_cell.inner", "textobjects") end)
 
 vim.api.nvim_create_autocmd('FileType', {
     pattern = {
