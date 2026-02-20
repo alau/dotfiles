2dc6e7dd7deabe14a83f59e39258f0b7c4ab7fb6
Andreas Launila <andreas.launila@instabee.com>
2026-02-20 16:37:05 +0100

    Configure some bindings

--- dot_config/nvim/lua/alau/plugin_configs/claudecode.lua
+++ dot_config/nvim/lua/alau/plugin_configs/claudecode.lua
@@ -1,3 +1,6 @@
 require("claudecode").setup({git_repo_cwd = true, terminal = {split_side = "left", provider = "snacks"}})
 
-vim.keymap.set({'n'}, '<leader>Co', '<cmd>ClaudeCode<cr>')
+vim.keymap.set('n', '<leader>Co', '<cmd>ClaudeCode<cr>')
+vim.keymap.set('n', '<leader>Cf', '<cmd>ClaudeCodeFocus<cr>')
+vim.keymap.set('n', '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>')
+vim.keymap.set('n', '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>')
