75f9f669ae4af575a0662c5e5412f69fd8bb27d4
Andreas Launila <andreas.launila@instabee.com>
2026-02-23 12:33:35 +0100

    Reword bindings

--- dot_config/nvim/lua/alau/plugin_configs/claudecode.lua
+++ dot_config/nvim/lua/alau/plugin_configs/claudecode.lua
@@ -2,5 +2,6 @@ require("claudecode").setup({git_repo_cwd = true, terminal = {split_side = "left
 
 vim.keymap.set('n', '<leader>Co', '<cmd>ClaudeCode<cr>')
 vim.keymap.set('n', '<leader>Cf', '<cmd>ClaudeCodeFocus<cr>')
-vim.keymap.set('n', '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>')
-vim.keymap.set('n', '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>')
+vim.keymap.set('n', '<leader>Ca', '<cmd>ClaudeCodeDiffAccept<cr>')
+vim.keymap.set('n', '<leader>Cd', '<cmd>ClaudeCodeDiffDeny<cr>')
+vim.keymap.set('n', '<leader>Cb', '<cmd>ClaudeCodeAdd %<cr>')
